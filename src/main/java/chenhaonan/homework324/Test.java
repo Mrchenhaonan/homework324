fd52554174d004d2b11c8ed50ee8de359b18bfe8
chenhaonan <3154997972@qq.com>
2020-03-24 19:48:23 +0800

    第二次提交

--- src/main/java/chenhaonan/homework324/Test.java
+++ src/main/java/chenhaonan/homework324/Test.java
@@ -3,5 +3,7 @@ package chenhaonan.homework324;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("helloWord");
+		System.out.println("helloWord");
+		System.out.println("helloWord");
 	}
 }
